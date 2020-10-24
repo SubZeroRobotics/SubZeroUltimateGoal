c0d941c88981ccafe2d25dfa2d9bc7c310b96e1c
Nihar Kodkani <nihar.kodkani@gmail.com>
2020-10-23 21:09:20 -0400

    Updated RR
    Updated RR

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/VisionByAverage.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/VisionByAverage.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.Autonomous.Vision;
+package org.firstinspires.ftc.teamcode.Autonomous;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
